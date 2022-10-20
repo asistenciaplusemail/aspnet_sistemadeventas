691a790ef0824b34d6e83c0170b9864ca8b0eea9
unknown <Administrador@SOPORTE-JULIO.mtssuy.local>
2022-10-20 16:32:46 -0300

    subiendopoyecto

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=azuresqlserverdata.database.windows.net;Initial Catalog=sqldatabaseserver;User ID=administrador;Password=Unity.901022;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
